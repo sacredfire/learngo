2d4febe44b7e84c412e78f384df9f37139207145
sacredfire <sergei.kissel@gmail.com>
2019-05-01 13:42:25 +0300

    change

--- hello/hello.go
+++ hello/hello.go
@@ -102,20 +102,6 @@ import "fmt"
 // 	fmt.Println(x[4])
 // }
 
-func main() {
-	var x [5]float64
-	x[0] = 98
-	x[1] = 93
-	x[2] = 77
-	x[3] = 82
-	x[4] = 83
-	var total float64 = 0
-	for i := 0; i < len(x); i++ {
-		total += x[i]
-	}
-	fmt.Println(total / float64(len(x)))
-}
-
 // func main() {
 // 	var x [5]float64
 // 	x[0] = 98
@@ -123,11 +109,25 @@ func main() {
 // 	x[2] = 77
 // 	x[3] = 82
 // 	x[4] = 83
-// 	var total float64
-// 	for _, value := range x {
-// 		total += value
+// 	var total float64 = 0
+// 	for i := 0; i < len(x); i++ {
+// 		total += x[i]
 // 	}
 // 	fmt.Println(total / float64(len(x)))
 // }
+
+func main() {
+	var x [5]float64
+	x[0] = 98
+	x[1] = 93
+	x[2] = 77
+	x[3] = 82
+	x[4] = 83
+	var total float64
+	for _, value := range x {
+		total += value
+	}
+	fmt.Println(total / float64(len(x)))
+}
 
 // some comment just to test commit via vsstudio
